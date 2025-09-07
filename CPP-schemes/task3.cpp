38f34ba6e788df1b0bbaedf7f6116f5bdda81a53
VolunteerAutumn <draqoshaimposter@gmail.com>
2025-09-07 12:52:44 +0300

    Create task3.cpp

---
+++ CPP-schemes/task3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main() {
+	std::setlocale(LC_ALL, "");
+	cout << "_________________________" << endl;
+	cout << "| Продам велосипед      |" << endl;
+	cout << "| Модель: Ibis Ripmo V2 |" << endl;
+	cout << "| Телефон: 096-835-09-95|" << endl;
+	cout << "| 096 | 096 | 096 | 096 |" << endl;
+	cout << "| 835 | 835 | 835 | 835 |" << endl;
+	cout << "| 09  | 09  | 09  | 09  |" << endl;
+	cout << "| 95  | 95  | 95  | 95  |" << endl;
+	cout << "|_____|_____|_____|_____|" << endl;
+	return 0;
+}
