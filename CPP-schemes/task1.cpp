6f6b55ce8d15e8e757162a5d4a9a6fbf8b62b368
VolunteerAutumn <draqoshaimposter@gmail.com>
2025-09-07 12:36:27 +0300

    Create task1.cpp

---
+++ CPP-schemes/task1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "\tTWENTY ONE PILOTS - Ride" << endl;
+	cout << "I just wanna stay in the sun where I find\nI know it's hard sometimes\nPieces of peace in the sun's peace of mind\nI know it's hard sometimes" << endl;
+	return 0;
+}
