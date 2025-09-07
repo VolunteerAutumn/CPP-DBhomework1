6b249c44a5ff57b920a942548a929305429dc4fb
VolunteerAutumn <draqoshaimposter@gmail.com>
2025-09-07 12:41:30 +0300

    Create task2.cpp

---
+++ CPP-schemes/task2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Every\n\thunter\n\t\twants\n\t\t\tto know\n\t\t\t\twhere\n\t\t\t\t\tsits\n\t\t\t\t\t\tpheasant" << endl;
+	return 0;
+}
